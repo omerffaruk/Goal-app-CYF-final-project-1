896ef2fe9865b3706bdf13870be68eec13773ca9
Dharma <dharmaguadeloupe@gmail.com>
2022-01-12 17:17:39 +0000

    created signup page

---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,41 @@
+import "./SignUp.css";
+
+const SignUp = () => (
+<div className="signup-ctn">
+	<h2>Create A New Account</h2>
+	<p>Come and join the HTCT community! Let's set up your account.</p>
+		<div className="s-form">
+		<form action="" method="post" className="signup-form">
+		<div className="signup-form">
+				<label htmlFor="name">Username</label>
+				<input
+					type="text"
+					name="username"
+					id="name"
+					placeholder="your name"
+				></input>
+			</div>
+			<div className="signup-form">
+				<label htmlFor="email">Email</label>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder="mail@abc.com"
+				></input>
+			</div>
+			<div className="signup-form">
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					placeholder="*********"
+				></input>
+			</div>
+		</form>
+	</div>
+</div>
+);
+
+export default SignUp;

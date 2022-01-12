0eb7e833dab0c8139b2923e88cbc9d89a8a3857d
Dharma <dharmaguadeloupe@gmail.com>
2022-01-12 17:21:32 +0000

    finalised password section with styles

--- client/src/pages/Components/Password.js
+++ client/src/pages/Components/Password.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Password = () => {
   return (
-    <div>
+    <div className="password-ctn">
+      <div>
       <input type="checkbox" id="remember-user" name="remember" checked></input>
       <label htmlFor="remember">Remember me</label>
-      <a href="#">Forgot Password</a>
+      </div>
+      <a href="#">Forgot Password ?</a>
     </div>
   );
 };

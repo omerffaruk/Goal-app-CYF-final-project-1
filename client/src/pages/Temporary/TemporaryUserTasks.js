15eef9908dd6b83aad5cae04c38567ad2b6fb2e4
omerffaruk <farukyamak11044@gmail.com>
2022-01-13 17:52:34 +0000

    feat: create temporary user tasks page for /temporary/tasks/:username end point

---
+++ client/src/pages/Temporary/TemporaryUserTasks.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./TemporaryTasksStyling.css";
+
+function TemporaryUserTasks() {
+	const location = useLocation();
+	const { username } = useParams();
+	const { userTasks } = location.state;
+	console.log(userTasks);
+
+	const usersAllTasks = userTasks.map((task, i) => {
+		return (
+			<div key={task.taskid} className="each-task">
+				<h2>Task {i + 1}</h2>
+				<ul>
+					<li>Date: {task.date}</li>
+					<li>Task: {task.task}</li>
+					<li>Is task completed: {task.iscomplete ? "Yes! Well done" : "Not yet"}</li>
+					<li>Role: {task.role}</li>
+				</ul>
+			</div>
+		);
+	});
+
+	return (
+		<div className="user-tasks-container">
+			<h1 className="title">{username}'s Tasks</h1>
+			{usersAllTasks}
+			<h3>
+				<Link className="temporary-link" to={"/"}>Go back to Home Page</Link>
+			</h3>
+			<h3>
+				<Link className="temporary-link" to={"/temporary/tasks"}>Go back to Tasks Page</Link>
+			</h3>
+		</div>
+	);
+}
+
+export default TemporaryUserTasks;

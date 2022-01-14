f19ccd6051d5b6a17114d921368151436cd69780
Dharma <dharmaguadeloupe@gmail.com>
2022-01-14 10:50:49 +0000

    added signup page routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-
-
+import NavBar from "./pages/Components/NavBar";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
   );

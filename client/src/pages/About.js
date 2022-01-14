061ea3c074d80c0d02d5e1fd8b6af16c20b8496a
Dharma <dharmaguadeloupe@gmail.com>
2022-01-14 15:13:22 +0000

    about page created

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,13 +1,30 @@
 const About = () => (
 	<main role="main">
 		<div>
-			<h1>About</h1>
+			<h2>About</h2>
 			<p>
-				Starter kit for full-stack JavaScript projects. For more information,
-				see the wiki:
+				As part of the Scrum process in the Agile methodology, teams/trainees are encouraged to perform a Daily Standup meeting. The Daily Standup is a brief meeting where team members discuss what's going on in their current work sprint to see if any issues have come up. Keep track of your tasks with the Goal! app created by HTCT.
 			</p>
-			<a href="https://github.com/textbook/starter-kit/wiki">Wiki</a>
+			<h4>What is the Daily Stand-Up?</h4>
+			<ul>
+				<li>15 minutes, same time every day</li>
+				<li>3 Questions:
+					<ul>
+						<li>What Did I accomplish yesterday?</li>
+						<li>What do I plan to accomplish today?</li>
+						<li>What are my blockers(issues)?</li>
+						</ul>
+						</li>
+						</ul>
 		</div>
+		<div className="team-ctn">
+		<p>Meet the team HTCT</p>
+		<img src={require("../images/anza.png")} alt="Anza Azam"></img>
+		<img src={require("../images/dharma.png")} alt="Dharma Emmelene"></img>
+		<img src={require("../images/musa.png")} alt="Musa Yuksel"></img>
+		<img src={require("../images/omer.png")} alt="Omer Faruk"></img>
+		</div>
+		<p>Dharma is a NW4 trainee, former theatre kid turned coder.</p>
 	</main>
 );
 

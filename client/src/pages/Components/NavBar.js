6e5c9f4faec1f84219db5ae212e72dbfca557f7e
omerffaruk <farukyamak11044@gmail.com>
2022-01-14 21:29:15 +0000

    refactor: turn logo into a homepage link

--- client/src/pages/Components/NavBar.js
+++ client/src/pages/Components/NavBar.js
@@ -2,14 +2,20 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="header">
-      <img
-        className="logo"
-        src={require("../../images/placeholder_logo.png").default}
-        alt=""
-      ></img>
-      <img className="avatar" src={require("../../images/avatar.png").default} alt=""></img>
-    </div>
-  );
+		<div className="header">
+			<a href="/">
+				<img
+					className="logo"
+					src={require("../../images/placeholder_logo.png").default}
+					alt=""
+				/>
+			</a>
+			<img
+				className="avatar"
+				src={require("../../images/avatar.png").default}
+				alt=""
+			></img>
+		</div>
+	);
 };
 export default NavBar;

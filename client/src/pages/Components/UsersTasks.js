dccb173d153c950dfb9ada33fe6c94239c7b512c
musayuksel <musayuxel@gmail.com>
2022-01-18 11:25:51 +0000

    fix:correct component route

--- client/src/pages/Components/UsersTasks.js
+++ client/src/pages/Components/UsersTasks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Checkbox from "../Components/Checkbox";
+import Checkbox from "./Checkbox";
 function UsersTasks() {
 	const [tasks, setTasks] = useState([]);
 	const [todayTasks, setTodayTasks] = useState("");

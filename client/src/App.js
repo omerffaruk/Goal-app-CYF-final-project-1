a852fdd2872d70e7a8d03934963491a63e9df0af
Dharma <dharmaguadeloupe@gmail.com>
2022-01-19 13:24:23 +0000

    fix: route to forgotpassword component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import TemporaryUserTasks from "./pages/Temporary/TemporaryUserTasks";
 import UsersTasks from "./pages/Components/UsersTasks";
 import NavBar from "./pages/Components/NavBar";
 import About from "./pages/About";
+import ForgotPassword from "./pages/ForgotPassword";
 
 import Home from "./pages/Home";
 
@@ -17,7 +18,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/signup" element={<SignUp />} />
 				<Route path="/about" element={<About />} />
-
+				<Route path="/forgot" element={<ForgotPassword />} />
 				{/* -----------Temporary Routes--------------- */}
 				<Route path="/temporary/tasks" element={<TemporaryTasks />} />
 				{/* <Route path="/temporary/tasks/:userName" element={<UsersTasks />} /> */}

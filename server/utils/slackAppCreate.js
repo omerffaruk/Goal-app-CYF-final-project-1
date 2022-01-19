ec8b2617235b2e35e6493dbe7aafc84d2a60940f
omerffaruk <farukyamak11044@gmail.com>
2022-01-19 18:22:53 +0000

    refactor: work the .env file in working directory

--- server/utils/slackAppCreate.js
+++ server/utils/slackAppCreate.js
@@ -1,9 +1,11 @@
 import { App, ExpressReceiver } from "@slack/bolt";
 import dotenv from "dotenv";
-dotenv.config({ path: __dirname + "/.env" });
+dotenv.config({ path: process.cwd() + "/.env" });
 export const createApp = (appName) => {
   const signingSecret = process.env.SLACK_SIGNING_SECRET;
+  console.log({ signingSecret });
   const token =process.env.SLACK_BOT_TOKEN;
+  console.log({ token });
   const receiver = new ExpressReceiver({
     signingSecret,
     endpoints: {

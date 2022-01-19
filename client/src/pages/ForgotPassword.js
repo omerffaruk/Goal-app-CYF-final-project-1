750662dd4fab73ffa9e8f1d2a177d9295c93a3b1
omerffaruk <farukyamak11044@gmail.com>
2022-01-19 16:26:22 +0000

    feat: add classnames for styling

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -1,21 +1,36 @@
 import React from "react";
 
 const ForgotPassword = () => {
-  return (
-    <div>
-      <div>
-      <h3>Forgot your password?</h3>
-      <p>Please enter the email address registered for your account. A verification code will be sent. You will be able to choose a new password for your account</p>
-    </div>
-    <div>
-    <form>
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" placeholder="mail@abc.com" required></input>
-    </form>
-    </div>
-    <div><button type="button">Reset</button></div>
-    </div>
-  );
+	return (
+		<div className="forgot-psw-ctn">
+			<div>
+				<h3>Forgot your password?</h3>
+				<p>
+					Please enter the email address registered for your account. A
+					verification code will be sent. You will be able to choose a new
+					password for your account.
+				</p>
+			</div>
+			<div>
+				<form>
+					<div className="form-field">
+						<div className="label-ctn">
+							<label htmlFor="email">Email</label>
+						</div>
+						<input
+							type="email"
+							id="email"
+							placeholder="mail@abc.com"
+							required
+						></input>
+					</div>
+				</form>
+			</div>
+			<div>
+				<button type="button" className="login-btn">Reset</button>
+			</div>
+		</div>
+	);
 };
 
 export default ForgotPassword;

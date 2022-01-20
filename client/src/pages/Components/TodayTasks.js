7dd575d86c24cd86986be8558a750ab8d4e46211
musayuksel <musayuxel@gmail.com>
2022-01-20 16:01:27 +0000

    refactor:Separate add new task from today's task

--- client/src/pages/Components/TodayTasks.js
+++ client/src/pages/Components/TodayTasks.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function TodayTasks({
+export function TodayTasks({
 	task,
 	setTodayTasks,
 	handleAddNewTask,
@@ -9,31 +9,54 @@ export default function TodayTasks({
 	const [todayValue, setTodayValue] = useState(task.task);
 	function handleChange(event) {
 		setTodayValue(event.target.value);
-		setTodayTasks((prev) =>
-			prev.map((currentTask, currentIndex) => {
-				if (currentIndex === index) {
-					currentTask.task = event.target.value;
-				}
-				return currentTask;
-			})
-		);
+		setTodayTasks((prev) => {
+			const updatedData = [...prev];
+			updatedData[index].task = event.target.value;
+			return updatedData;
+		});
 	}
+
 	return (
 		<li>
 			<input
 				type="text"
-				autoFocus
 				name="today"
 				placeholder="Enter for new task..."
 				value={todayValue}
 				onChange={handleChange}
+				// disabled={true}
 				onKeyPress={(event) => {
 					if (event.key === "Enter") {
 						event.preventDefault();
-						handleAddNewTask();
+						handleChange(event);
 					}
 				}}
 			/>
 		</li>
+	);
+}
+//ADD NEW TASK
+export function NewTask({ handleAddNewTask }) {
+	const [todayValue, setTodayValue] = useState("");
+	function handleChange(event) {
+		setTodayValue(event.target.value);
+	}
+
+	return (
+		<input
+			type="text"
+			autoFocus
+			name="newTask"
+			placeholder="Enter for new task..."
+			value={todayValue}
+			onChange={handleChange}
+			// disabled={true}
+			onKeyPress={(event) => {
+				if (event.key === "Enter") {
+					event.preventDefault();
+					handleAddNewTask(todayValue);
+				}
+			}}
+		/>
 	);
 }

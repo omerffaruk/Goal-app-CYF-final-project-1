a4cac05a676516ba3ea55346cfee71123c9f9e03
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2022-01-24 12:49:25 +0000

    sign in slack feat

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,7 @@ import UsersTasks from "./pages/Components/UsersTasks";
 import NavBar from "./pages/Components/NavBar";
 import About from "./pages/About";
 
-
-import ResetPassword from "./pages/Components/ResetPassword"
+import ResetPassword from "./pages/Components/ResetPassword";
 
 import Home from "./pages/Home";
 import Password from "./pages/Components/Password";
@@ -25,9 +24,21 @@ function App() {
 				<Route path="/forgot" element={<ForgotPassword />} />
 
 				{/* -----------Temporary Routes--------------- */}
-				{<Route path="/temporary/tasks" element={<TemporaryTasks />} />}
-				{/* <Route path="/temporary/tasks/:userName" element={<UsersTasks />} /> */}
-				{<Route path="/:username" element={<UsersTasks />} />}
+				<Route path="/temporary/tasks" element={<TemporaryTasks />} />
+				<Route path="/:username" element={<UsersTasks period={"daily"} />} />
+				<Route
+					path="/:username/weekly"
+					element={<UsersTasks period={"weekly"} />}
+				/>
+				<Route
+					path="/:username/monthly"
+					element={<UsersTasks period={"monthly"} />}
+				/>
+				<Route
+					path="/:username/quarterly"
+					element={<UsersTasks period={"quarterly"} />}
+				/>
+
 				{<Route path="/forgot_password" element={<Password />} />}
 				{<Route exact path="/reset_password/:id" element={<ResetPassword />} />}
 			</Routes>

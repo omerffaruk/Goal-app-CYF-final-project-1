a0980234fb5629858beff8d4e675735a7eb8b338
omerffaruk <farukyamak11044@gmail.com>
2022-01-25 15:28:59 +0000

    feat: update errorDisplay with login fetch

--- client/src/pages/Components/LoginBtn.js
+++ client/src/pages/Components/LoginBtn.js
@@ -4,7 +4,7 @@ import Popup from "./Popup.js";
 import fetchData from "../../utils/fetchData.js";
 import { headers } from "../../utils/generalPostObjects.js";
 
-const LoginBtn = ({ email, password }) => {
+const LoginBtn = ({ email, password, setErrorDisplay }) => {
 	const [text, setText] = useState("");
 	const [popups, setPopups] = useState(false);
 	const [login, setLogin] = useState(false);
@@ -19,17 +19,18 @@ const LoginBtn = ({ email, password }) => {
 				password: password,
 			}),
 		};
-		fetchData(`/log`, methodObj)
+		fetchData("/log", methodObj)
 			.then((res) => res.json())
 			.then((data) => {
 				console.log({ data });
 				if (data.user) {
 					localStorage.setItem("t", data.user);
+					setErrorDisplay(false);
 					setLogin(true);
 					navigate(`/${data.username}`);
 				} else {
 					//setText("usernamepassword");
-					window.alert(text);
+					 setErrorDisplay(true);
 				}
 			})
 			.catch((e) => console.log(e));
